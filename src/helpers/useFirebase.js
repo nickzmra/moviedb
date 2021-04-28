cd3e8bb9160c22f405569db874d9676ded50a013
nickzmra <nzamora9@stu.southtexascollege.edu>
2021-04-27 19:03:11 -0500

    Added Comment section

--- src/helpers/useFirebase.js
+++ src/helpers/useFirebase.js
@@ -1,18 +1,50 @@
+import { ref, onUnmounted, onMounted } from "vue"
 import firebase from "firebase/app"
 import { useAuth } from "@vueuse/firebase"
 import "firebase/auth"
+import "firebase/firestore"
 
 import { firebaseConfig } from "~/config/firebase"
 
 firebase.initializeApp(firebaseConfig)
 
-const { auth } = firebase
+const { auth, firestore } = firebase
 const { GoogleAuthProvider } = auth
+const db = firestore()
 
 const { isAuthenticated, user } = useAuth()
 
 export const authentication = () => {
-	const signIn = () => auth().signInWithPopup(new GoogleAuthProvider())
-	const signOut = () => auth().signOut()
-	return { signIn, signOut, isAuthenticated, user }
+  const signIn = () => auth().signInWithPopup(new GoogleAuthProvider())
+  const signOut = () => auth().signOut()
+  return { signIn, signOut, isAuthenticated, user }
+}
+
+export const database = (movieId) => {
+  const comments = ref([])
+  const commentsCollection = db.collection("comments")
+  const commentsQuery = commentsCollection
+    .where("movieId", "==", movieId)
+    .orderBy("createdAt")
+
+  const unsubscribe = commentsQuery.onSnapshot((c) => {
+    comments.value = c.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+  })
+
+  onUnmounted(unsubscribe)
+
+  const addComment = async (text) => {
+    if (!isAuthenticated.value) return
+    const { uid, displayName } = user.value
+
+    await commentsCollection.add({
+      userName: displayName,
+      userId: uid,
+      movieId,
+      text,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+    })
+  }
+
+  return { comments, addComment }
 }
